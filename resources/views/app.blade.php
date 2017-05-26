bbbf26a57d51313f5aa165f4a059d6ce40e2ffbc
leexikang <leexikang@gmail.com>
2017-05-26 13:40:05 +0630

    hot fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
         @yield('content')
         </div>
    </body>
-   <script src="{{ assets('js/app.js') }}"></script>
+   <script src="{{ asset('js/app.js') }}"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
    <script>
      
