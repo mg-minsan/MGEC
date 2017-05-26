c760f15155202b2ec473f8fb194bbdad3c27102f
leexikang <leexikang@gmail.com>
2017-05-26 13:59:16 +0630

    add seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        factory(App\User::class, 10)->create();
-        factory(App\Attendance\Attendance::class, 10)->create();
-        factory(App\Attendance\Trip::class, 10)->create();
-        factory(App\Attendance\RollCall::class, 10)->create();
+       /* factory(App\User::class, 10)->create();*/
+        //factory(App\Attendance\Attendance::class, 10)->create();
+        //factory(App\Attendance\Trip::class, 10)->create();
+        /*factory(App\Attendance\RollCall::class, 10)->create();*/
+        $users = ['Min San', 'Kyaw Kyar', 'Soe Yan Naung', 
+            'Thet Oo Zin', 'Banyar', 'Hsu Way Phyo', 'Yan Lin Aung',
+            'Sein Khat Soe', 'Kaung Htet Thu', 'Phyow Nya',
+            'Lily Phyo', 'Aung Pyae Sone'];
+
+        DB::table('trips')->insert([
+            'name' => 'Shan Yoma 2017',
+            'duration' => '22',
+            'created_at' => date('Y-m-d h:i:s', time()),
+            'updated_at' => date('Y-m-d  h:i:s', time()),
+
+        ]);
+
+        foreach($users as $user){
+
+            DB::table('attendances')->insert([
+                'name' => $user,
+                'created_at' => date('Y-m-d h:i:s', time()),
+                'updated_at' => date('Y-m-d h:i:s', time()),
+            ]);
+        }
+
+
     }
 }
